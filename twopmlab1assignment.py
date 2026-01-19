7fbc5b154a57cf256003d5299cd310e026cfa6b6
tracyny1212-ctrl <tracyn1212@gmail.com>
2026-01-19 15:07:00 -0500

    Add files via upload

---
+++ twopmlab1assignment.py
@@ -0,0 +1,15 @@
+#P1
+def myHello_world():
+    print("Hello, World!")
+
+myHello_world()
+#P2
+def input_output():
+    n=input("Enter your name: ")
+    a= input("Enter your age: ")
+    h= input("Enter your height:(in meters) ")
+
+    print(f"Hello, {n}!")
+    print(f"You are {a} years old.")
+    print(f"Your height is {h} meters.")
+input_output()
